dea0fcf925091b319a01245e959e59544c3162b9
Maxime Verriest <maxime.verriest@quadra-informatique.fr>
2024-07-24 11:20:15 +0200

    base section html home page

--- public/assets/js/function.js
+++ public/assets/js/function.js
@@ -1,6 +1,19 @@
 
 
-/* Please ❤ this if you like it! */
+
+(function () {
+    $(window).scroll(function () { 
+        var Num = $(window).scrollTop() / 500;
+        var Num2 = $(window).scrollTop() * .0004; // higher number for more zoom
+        var Num2mod = Num2 + 1;
+        var Num3 = $(window).scrollTop() * .2; // Title speed
+        var Num3mod = Num3 + 1;
+        return $('.shade').css('opacity', Num),
+        $(".bg").css({"transform":"scale(" + Num2mod + ")"}),
+        $(".text").css({"margin-top":"-" + Num3mod + "px"});
+    });
+}.call(this));
+
 
 
 (function($) { "use strict";
