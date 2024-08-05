cfff6bc5dbd0844e095399b6041c83670675aa43
Guillaume Crégut <gcregut@free.fr>
2024-08-05 12:03:17 +0200

    [Feat]: ProductManager : add getProductsByCategory

--- src/Model/ProductManager.php
+++ src/Model/ProductManager.php
@@ -7,4 +7,17 @@ use PDO;
 class ProductManager extends AbstractManager
 {
     public const TABLE = 'product';
+
+    public function getProductByCategory(int $categoryId): array
+    {
+        $query = "SELECT p.name as productName, p.id as productid, p.price,
+         p.description, p.updated_at ,p.image, p.descriptionDetail,
+          c.name as categoryName FROM product p INNER JOIN category c
+          ON p.category_id=c.id 
+          WHERE c.id=:id";
+        $stm = $this->pdo->prepare($query);
+        $stm->bindValue(':id', $categoryId, PDO::PARAM_INT);
+        $stm->execute();
+        return $stm->fetchAll();
+    }
 }
