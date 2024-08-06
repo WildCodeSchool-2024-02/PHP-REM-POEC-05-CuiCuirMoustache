3396f726eff60d59857d198efa17b944afea6ceb
Guillaume Crégut <gcregut@free.fr>
2024-08-06 12:58:40 +0200

    [Feat]: CategorController: add subcategory products

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -25,8 +25,12 @@ class CategoryController extends AbstractController
         } else {
             $categoryName = $category['name'];
             $description = $category['description'];
-            $productsManager = new ProductManager();
-            $products = $productsManager->getProductByCategory($id);
+            $productsCategory = $this->getProducts($id);
+            //Get subcategories
+            $subProducts = $this->getSubCategoryProducts($id);
+            //Merge subproducts with products
+            $products = array_merge($subProducts, $productsCategory);
+            // Get all categories menu (for breadcrumb)
             $categoriesMenu = $categoryManager->selectAll();
         }
         return $this->twig->render('Category/index.html.twig', [
@@ -36,5 +40,23 @@ class CategoryController extends AbstractController
             'description' => $description,
             'categories' => $categoriesMenu
         ]);
+    }
+
+    private function getSubCategoryProducts(int $categoryId): array
+    {
+        $subProducts = [];
+        $categorieManager = new CategorieManager();
+        $subCategories = $categorieManager->getSubCategories($categoryId);
+        foreach ($subCategories as $subCategory) {
+            $subProducts = array_merge($subProducts, $this->getProducts($subCategory['id']));
+        }
+        return $subProducts;
+    }
+
+    private function getProducts(int $category): array
+    {
+        $productsManager = new ProductManager();
+        $products = $productsManager->getProductByCategory($category);
+        return $products;
     }
 }
